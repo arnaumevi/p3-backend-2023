e206ce33526a69e282502d3ce913bd94fffd72a2
Arnau Merino <arnaumevi@gmail.com>
2023-05-21 13:51:01 +0200

    Testing books api GET

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 
@@ -10,8 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", async (req, res) => {
-  res.status(200).send("hi!");
+app.get("/books", async (req, res) => {
+  try {
+    const result = await prisma.book.findMany({});
+    res.status(200).json(result);
+  } catch (e) {
+    res.status(500).send({
+      type: e.constructor.name,
+      message: e.toString(),
+    });
+  }
 });
 
 const { SERVER_PORT } = process.env;
